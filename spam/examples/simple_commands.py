474a5fb102541fada798038174509120d29334e9
bdastur <bdastur@gmail.com>
2015-11-09 03:14:43 -0800

    Add a user_pass parameter to perform operation. Add a new api to return dark hosts

--- spam/examples/simple_commands.py
+++ spam/examples/simple_commands.py
@@ -10,14 +10,19 @@ def execute_ping():
     Execute ls on some hosts
     '''
     runner = ansirunner.AnsibleRunner()
-    host_list = ["10.163.41.4"]
-    remote_user = "root"
+    host_list = ["11.111.11.1", "11.122.22.2", "12.133.31.32"]
+    remote_user = "3444"
+    remote_pass = "4eeeeeel1e3"
     result, failed_hosts = runner.ansible_perform_operation(
         host_list=host_list,
         remote_user=remote_user,
+        remote_pass=remote_pass,
         module="ping")
 
     print result, failed_hosts
+    dark_hosts = runner.ansible_get_dark_hosts(result)
+    print "dark hosts: ", dark_hosts
+
 
 def main():
     '''
