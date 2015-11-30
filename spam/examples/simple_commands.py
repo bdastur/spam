18adde4dfc54e2006113e0eebe1c1d8abe878abc
bdastur <bdastur@gmail.com>
2015-11-30 04:18:02 -0800

    Modify example to use cmd line args.

--- spam/examples/simple_commands.py
+++ spam/examples/simple_commands.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import os
+import argparse
+import getpass
 import spam.ansirunner
 import spam.plugins.rados as rados
 
-host_list = ["11.111.11.1", "11.113.11.1", "11.111.11.13"]
-remote_user = "wwot"
-remote_pass = "wwwwwwwwww3"
 
-def execute_ping():
+def execute_ping(host_list, remote_user, remote_pass):
     '''
     Execute ls on some hosts
     '''
@@ -25,7 +24,7 @@ def execute_ping():
     print "dark hosts: ", dark_hosts
 
 
-def execute_ls():
+def execute_ls(host_list, remote_user, remote_pass):
     '''
     Execute any adhoc command on the hosts.
     '''
@@ -40,7 +39,7 @@ def execute_ls():
     print "Result: ", result
 
 
-def execute_rados_df():
+def execute_rados_df(host_list, remote_user, remote_pass):
     runner = spam.ansirunner.AnsibleRunner()
     result, failed_hosts = runner.ansible_perform_operation(
         host_list=host_list,
@@ -52,29 +51,52 @@ def execute_rados_df():
     print "Result: ", result
 
 
-def execute_rados_df2():
+def execute_rados_df2(host_list, remote_user, remote_pass):
     rados_handle = rados.Rados()
-    result = rados_handle.rados_df(host_list=["10.163.41.4"],
+    result = rados_handle.rados_df(host_list=host_list,
                                    remote_user=remote_user,
                                    remote_pass=remote_pass)
 
-    #result, failed_hosts = rados_handle.perform_operation(
-    #    host_list=["10.163.41.4"],
-    #    remote_user=remote_user,
-    #    remote_pass=remote_pass,
-    #    cmd="rados df")
-    print "RESULTS: ", result['contacted']['10.163.41.4']['parsed_results']
+    print "RESULTS: ", result['contacted']
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog="cloudops.py",
+                                     description="CPE Cloud Ops ")
+
+    parser.add_argument("--hosts",
+                        help="Hosts to execute operations on",
+                        nargs='+',
+                        required=True)
+    parser.add_argument("--username",
+                        help="Username for remote hosts",
+                        required=True)
+    parser.add_argument("--ask-pass",
+                        help="Ansible operation will prompt for user password",
+                        action="store_true")
+
+    args = parser.parse_args()
+    return args
 
 
 def main():
     '''
     Simple examples
     '''
-    execute_ping()
+    args = parse_arguments()
+    if args.askpass:
+        password = getpass.getpass("Password: ")
+
+    if not args.username:
+        username = getpass.getuser()
+
+    host_list = args.remote_hosts
+    os.environ["ANSIBLE_HOST_KEY_CHECKING"] = "False"
+
+    execute_ping(host_list, username, password)
     #execute_ls()
     #execute_rados_df()
-    execute_rados_df2()
+    execute_rados_df2(host_list, username, password)
 
 
 if __name__ == '__main__':
